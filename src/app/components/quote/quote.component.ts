135b3419dc069008fa37d56cca918733555b0c4b
Alexotieno1717 <alexotieno900@gmail.com>
2020-08-23 15:49:53 +0300

    Added quotes

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../../quotes';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quote:Quotes[]=[
+    new Quotes(1, 'Alex Otieno', 'Web Hosting', 'Website of all kinds are posted on cloud to facilitate many to access'),
+    new Quotes(2, 'Alex', 'Tech and life', 'For a list of all the ways technology has failed to improve the quality of life, please press three'),
+    new Quotes(3, 'James', 'Technology', 'Technology… the knack of so arranging that we don’t have to experience it.'),
+  ]
+
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.quote.splice(index,1);
+    }
+  }
+
+  toggleDetails(index){
+    this.quote[index].showDescription = !this.quote[index].showDescription;
+  }
 
   constructor() { }
 
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    showDescription: boolean;
+    constructor(public id:number, public name:string,  public quoteTitile:string,  public quoteMessage:string){ 
+        this.showDescription=false;
+    }
 }
